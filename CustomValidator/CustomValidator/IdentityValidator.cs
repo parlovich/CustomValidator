ee388753a3db4133781b0df77857af5b36bab934
Uladzimir Tsikhon <uladzimir_tsikhon@epam.com>
2018-10-30 14:43:30 +0300

    Add Customvalidator skeleton

---
+++ CustomValidator/CustomValidator/IdentityValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CustomValidator
+{
+    public class IdentityValidator
+    {
+        public IdentityValidator()
+        {
+        }
+        public bool Validate(string pwd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomValidator/CustomValidator/IdentityValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+namespace CustomValidator
+{
+    [TestFixture]
+    public class IdentityValidatorTests
+    {
+        [Test]
+        public void ValidateTestCase()
+        {
+            var validator = new IdentityValidator();
+            var expected = true;
+            var actual = validator.Validate("fakedata");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
